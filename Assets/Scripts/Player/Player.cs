40bb2ed466b84b4f5fbf03fb98495ee9f35964ec
michellimadesigner <michellimadesigner@gmail.com>
2023-09-10 20:05:05 -0300

    wip: created the basic of player scripts

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+  // Player properties
+  public float speed;
+  public float jumpForce;
+  private float move;
+  public PlayerScriptable attributes;
+
+  // Actions controls
+  private bool isJumping = false;
+  private float direction;
+  private bool isFacingRight = true;
+
+  // Components
+  private Rigidbody2D rig;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    rig = GetComponent<Rigidbody2D>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    move = Input.GetAxis("Horizontal");
+    // Moviment
+    Move();
+    Flip();
+    Jump();
+  }
+
+  // -------------------- Player moviment
+  void Move()
+  {
+    Vector3 movement = new Vector3(move, 0f, 0f);
+    transform.position += movement * Time.deltaTime * speed;
+  }
+
+  void Flip()
+  {
+    if (move > 0 && !isFacingRight)
+    {
+      isFacingRight = !isFacingRight;
+      transform.Rotate(0f, 180f, 0f);
+    }
+    else if (move < 0 && isFacingRight)
+    {
+      isFacingRight = !isFacingRight;
+      transform.Rotate(0f, 180f, 0f);
+    }
+  }
+
+  void Jump()
+  {
+    if (Input.GetButtonDown("Jump") && !isJumping)
+    {
+      rig.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+    }
+  }
+
+  // -------------------- Damage and Die
+  public void TakeDamage(int playerDamage)
+  {
+    attributes.health -= playerDamage;
+
+    if (attributes.health <= 0)
+    {
+      Die();
+    }
+  }
+
+  void Die()
+  {
+    Destroy(gameObject);
+  }
+}
---
+++ Assets/Scripts/Player/PlayerScriptable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerData", menuName = "PlayerScriptable", order = 0)]
+public class PlayerScriptable : ScriptableObject
+{
+  public int health = 8;
+  // public potionOne;
+  // public potionTwo;
+}
