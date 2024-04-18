b80e3ba32af0379370258d69227803bccc5ba395
michellimadesigner <michellimadesigner@outlook.com>
2024-04-17 22:57:02 -0300

    refactor: remove multiples potion spawns

--- Assets/Scripts/Items/PotionSpawner.cs
+++ Assets/Scripts/Items/PotionSpawner.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 public class PotionSpawner : MonoBehaviour
 {
   public GameObject[] potionsPrefabs;
-  // public List<Transform> spawnLocations = new List<Transform>();
   public Transform spawnPoint;
 
   public float spawnInterval = 3.0f;
@@ -17,27 +16,8 @@ public class PotionSpawner : MonoBehaviour
 
   IEnumerator SpawnPotions()
   {
-    if (!LocationHasPotion())
-    {
-      yield return new WaitForSeconds(spawnInterval);
-      SpawnPotion();
-    }
-  }
-
-  bool LocationHasPotion()
-  {
-    // Check if a potion already exists at the given position
-    Collider2D[] colliders = Physics2D.OverlapCircleAll(spawnPoint.position, 0.5f); // Adjust the radius as needed
-
-    foreach (Collider2D collider in colliders)
-    {
-      if (collider.CompareTag("Item"))
-      {
-        return true;
-      }
-    }
-
-    return false;
+    yield return new WaitForSeconds(spawnInterval);
+    SpawnPotion();
   }
 
   void SpawnPotion()
@@ -48,16 +28,5 @@ public class PotionSpawner : MonoBehaviour
     // Instantiate the potion at the chosen spawn location
     GameObject newPotion = Instantiate(randomPotionPrefab, spawnPoint.position, Quaternion.identity);
     // Add more customizations here if needed
-  }
-
-  void OnTriggerEnter2D(Collider2D others)
-  {
-    if (others.CompareTag("Player"))
-    {
-      if (!LocationHasPotion())
-      {
-        StartCoroutine(SpawnPotions());
-      }
-    }
   }
 }
